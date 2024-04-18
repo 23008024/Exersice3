c2c9dd1889be975eb457feddd25ca7600bcb0fd9
23008024 <163560278+23008024@users.noreply.github.com>
2024-04-18 20:02:19 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout <<"Choose how you prefer your future partner(Boyfriend) should be"<< endl;
+   int a=1;
+    cout<<" 1 GOOD PARTNER "<<endl;
+    cout<<"*..Good communication"<<endl;
+cout<<"*..Respectful partner"<<endl;
+cout<<"*..Supportive partner"<<endl;
+cout<<"*..A trustful partner"<<endl;
+cout<<""<<endl;
+cout<<""<<endl;
+
+cout<<"*2 BAD PARTNER"<<endl;
+
+cout<<"*..Bad communication"<<endl;
+cout<<"*.. Doesn't respect"<<endl;
+cout<<"*..Doesn't Respect"<<endl;
+cout<<"please choose a partner"<<endl;
+
+
+int e;
+cin>>e;
+if(e==1){
+cout<<"User prefer a Good partner without choosing his physical appearance"<<endl;}
+else if(e==2){
+cout<<"User prefer a Bad partner without choosing his physical appearance"<<endl;
+}
+int f;
+
+
+
+cout<<"1.Tall partner "<<endl;
+cout<<" Do you want a ..... 1.yes /2 No"<<endl;
+cin>>f;
+
+if(f==1){
+    cout<<"User want also a partner who is Tall"<<endl;
+}
+else if(f==2){
+    cout<<" user doest want a Tall partner"<<endl;
+}
+
+
+cout<<"1.weight "<<endl;
+cout<<" Do you want a ..... 1.yes /2 No"<<endl;
+cin>>f;
+
+if(f==1){
+    cout<<"User want also a partner who has big weight"<<endl;
+}
+else if(f==2){
+    cout<<" user doest want a partner who has a big weight"<<endl;
+}
+
+
+
+
+
+
+
+
+    return 0;
+}
